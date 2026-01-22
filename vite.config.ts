0e7c76fc1f9d45d78b31703b4e448a2466bc79d0
WeiHanChen <weihanchen0725@gmail.com>
2026-01-21 23:19:58 -0800

    Removed taiwindcss configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [sveltekit()],
 
 	test: {
 		expect: { requireAssertions: true },
