f523326309e351f9054cbadad3bcf2a6090bfbd1
WeiHanChen <weihanchen0725@gmail.com>
2026-02-02 21:49:08 -0800

    feat: Enhance Footer component with styled border and background effects

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import './Footer.scss';
 </script>
-<footer class="footer">
+<footer class="footer footer-border">
     <p>© {new Date().getFullYear()} Wei-Han Chen. All rights reserved.</p>
 </footer>
